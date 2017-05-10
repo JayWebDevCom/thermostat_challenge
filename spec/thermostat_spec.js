25ee08c27012b539fc4cdb51a1ccef89ec65e6d2
JayWebDevCom <jaiye.s@gmail.com>
2017-05-10 17:20:41 +0100

    add tested up and down functionality

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -7,7 +7,19 @@ describe("Thermostat", function() {
   });
 
   it("initiates with a temperature function set to 20", function(){
-    expect(thermostat.temperature).toBe(20)
+    expect(thermostat._temperature).toBe(20)
   });
+
+  it('temperature can be increased with an up function', function(){
+    thermostat.up(5);
+    expect(thermostat._temperature).toBe(25)
+  });
+
+  it('temperature can be decreased with an up function', function(){
+    thermostat.down();
+    expect(thermostat._temperature).toBe(19)
+  });
+
+
 
 });
