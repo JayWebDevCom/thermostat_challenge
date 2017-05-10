5be90c19f58a3cf4fadaee1ffd19168f7463f472
JayWebDevCom <jaiye.s@gmail.com>
2017-05-10 18:05:30 +0100

    add tested powerSaveToggle and reset

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -7,17 +7,42 @@ describe("Thermostat", function() {
   });
 
   it("initiates with a temperature function set to 20", function(){
-    expect(thermostat._temperature).toBe(20)
+    expect(thermostat._temp).toBe(20)
   });
 
   it('temperature can be increased with an up function', function(){
     thermostat.up(5);
-    expect(thermostat._temperature).toBe(25)
+    expect(thermostat._temp).toBe(25)
   });
 
   it('temperature can be decreased with an up function', function(){
     thermostat.down();
-    expect(thermostat._temperature).toBe(19)
+    expect(thermostat._temp).toBe(19)
+  });
+
+  it("should initiate with  power save mode set to on", function(){
+    expect(thermostat._powerSave).toBe(true)
+    expect(thermostat._maxTemp).toBe(25)
+  });
+
+  it("should allow us to change power saving mode from on to off", function(){
+    thermostat.powerSaveToggle()
+    expect(thermostat._powerSave).toBe(false)
+  })
+
+  it("should change the maxTemperature based on powerSave toggle", function(){
+    thermostat.powerSaveToggle()
+    expect(thermostat._maxTemp).toBe(32)
+  })
+
+  it("should reset to 20 degrees when the reset function is called", function(){
+    thermostat.reset()
+    expect(thermostat._temp).toBe(20)
+  });
+
+  it("should reset to predefined when the reset function is called with a parameter", function(){
+    thermostat.reset(11)
+    expect(thermostat._temp).toBe(11)
   });
 
 
