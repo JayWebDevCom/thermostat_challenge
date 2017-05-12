96224aabf1e61363a944924e252a6de975b9566a
JayWebDevCom <jaiye.s@gmail.com>
2017-05-12 06:01:18 +0100

    add refactor

--- public/test.js
+++ public/test.js
@@ -12,31 +12,43 @@ $(document).ready(function(){
 
   $(".warning").text(sessionStorage.details);
 
-  $(".temp").text(t.whatIsTemp());
+  $(".temp").text(t.whatIsTemp() + '°C');
 
   $(".upButton").click(function(){
     try {
       t.up();
-      $(".temp").text(t.whatIsTemp());
+      $(".temp").text(t.whatIsTemp() + '°C');
+
+      if (t._temp >= t._minTemp && t._temp <= t._maxTemp) {
+        $(".warning").text('')
+      }
+
     }
     catch(err) {
-      $(".warning").text(err.message)
+      $(".warning").text(err.message);
+      $(".temp").text(t.whatIsTemp() + '°C');
     }
   });
 
   $(".downButton").click(function(){
     try {
       t.down();
-      $(".temp").text(t.whatIsTemp());
+      $(".temp").text(t.whatIsTemp() + '°C');
+
+      if (t._temp >= t._minTemp && t._temp <= t._maxTemp) {
+        $(".warning").text('');
+      }
+
     }
     catch(err) {
-      $(".warning").text(err.message)
+      $(".warning").text(err.message);
+      $(".temp").text(t.whatIsTemp() + '°C');
     }
   });
 
   $(".resetButton").click(function(){
     t.reset();
-    $(".temp").text(t.whatIsTemp());
+    $(".temp").text(t.whatIsTemp() + '°C');
   })
 
   $(".slider").click(function(){
@@ -53,12 +65,12 @@ $(document).ready(function(){
     var x = $(".citySelect option:selected").val()
     var link = "http://api.openweathermap.org/data/2.5/weather?q=" + x + "&appid=8d768cdf677e80b711b9f48b07da13fc&units=metric";
     var the_weather = $.get(link, function(data) {
-      $(".weather").text( 'Outside in ' + data.name + ' it is ' + Math.round(data.main.temp) + '°C' );
+      $(".weather").text( data.name + ' - ' + Math.round(data.main.temp) + '°C' );
     });
   });
 
   var the_weather = $.get("http://api.openweathermap.org/data/2.5/weather?q=London&appid=8d768cdf677e80b711b9f48b07da13fc&units=metric", function(data) {
-    $(".weather").text( 'Outside in ' + data.name + ' it is ' + Math.round(data.main.temp) + '°C' );
+    $(".weather").text( data.name + ' - ' + Math.round(data.main.temp) + '°C' );
   });
 
 
