946ea92c4a637482c10cd20435bfc6fbafdd752d
JayWebDevCom <jaiye.s@gmail.com>
2017-05-13 12:15:36 +0100

    move setfixtures function and define methods in fixtures

--- spec/thermostat_spec2.js
+++ spec/thermostat_spec2.js
@@ -1,16 +1,5 @@
 describe("Thermostat", function() {
 
-  function setUpHTMLFixture() {
-
-       setFixtures(
-         '<script>thermostat2 = new Thermostat;function thermostatDown(){thermostat2.down();}</script>'
-        +'  <input type="text" id="txtMessage">'
-        +'  <div class="downButton" onclick="thermostatDown()"></div>'
-        +'  <p class="output2">``</p>'
-                );
-     }
-
-
   beforeEach(function() {
     setUpHTMLFixture();
   });
@@ -19,8 +8,8 @@ describe("Thermostat", function() {
 
     it("click reduces temperature by 1 C", function(){
       $('.downButton').trigger( "click" );
-      $(".output2").text(thermostat2._temp);
-      expect($(".output2").text()).toEqual('19');
+      $(".output").text(thermostat2._temp);
+      expect($(".output").text()).toEqual('19');
     });
 
   });
